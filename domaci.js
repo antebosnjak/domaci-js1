194ef009e8f461caf3e9fcda420b65ed5051bcee
antebosnjak <46303997+antebosnjak@users.noreply.github.com>
2022-11-30 17:45:28 +0100

    domaci js-1

---
+++ domaci.js
@@ -0,0 +1,32 @@
+//Prvi zadatak
+var mojeGodine=49;
+var mojePseceGodine=mojeGodine/7;
+console.log(mojePseceGodine);
+
+
+
+
+//Drugi zadatak
+var mojeIme="Ante"
+var mojePrezime="Bošnjak"
+var mojeImePrezime=mojeIme + " " + mojePrezime;
+
+console.log(mojeImePrezime);
+
+//Treći zadatak
+var prviBroj=7;
+var drugiBroj=17;
+console.log(drugiBroj/prviBroj);
+
+//Četvrti zadatak
+var brojPolaznika=22;
+console.log(brojPolaznika);
+var brojPolaznika=brojPolaznika+2
+console.log(brojPolaznika);
+var brojPolaznika=brojPolaznika-3
+console.log(brojPolaznika);
+var brojPolaznika=brojPolaznika+5
+console.log(brojPolaznika);
+
+
+
